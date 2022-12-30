5d4cfcbad5c34ddae632f508ac5b7f0c2f39900d
Joseph Olabisi <dabigjoe6@gmail.com>
2022-12-30 19:00:16 +0000

    Add cors options

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import {startDb} from './src/config/database.js';
-import { errorHandler } from './src/middleware/errorHandler.js';
-import routes from './src/routes/index.js';
-
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import { startDb } from "./src/config/database.js";
+import { errorHandler } from "./src/middleware/errorHandler.js";
+import routes from "./src/routes/index.js";
 
 const app = express();
 
@@ -16,20 +15,23 @@ dotenv.config();
 startDb();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+const corsOptions = {
+  origin: process.env.CLIENT_BASE_URL,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 //middlewares
-app.use(cors());
-app.use(express.json())
+app.use(cors(corsOptions));
+app.use(express.json());
 app.use(urlencodedParser);
 
 //routes
-app.use('/',routes);
+app.use("/", routes);
 
 //Handling error
 app.use(errorHandler);
 
-
 const port = process.env.PORT || 3003;
-app.listen(port, function() {
-    console.log(`Server Listening on Port ${port}`);
+app.listen(port, function () {
+  console.log(`Server Listening on Port ${port}`);
 });
