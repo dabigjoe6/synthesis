5f86b9bb2c9b9c0b85faa9c35986b468dcff8d55
Joseph Olabisi <dabigjoe6@gmail.com>
2022-12-30 19:22:45 +0000

    log cors options

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
+console.log("CORS OPTIONS", corsOptions.origin)
+
 //middlewares
 app.use(cors(corsOptions));
 app.use(express.json());
